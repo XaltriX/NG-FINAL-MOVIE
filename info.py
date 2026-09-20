c06284cb8016b77a51c7cedf113837ac04ef9aff
XaltriX <129171425+XaltriX@users.noreply.github.com>
2026-09-20 14:21:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,8 +115,8 @@ POSTER_SPOILER= bool(environ.get('POSTER_SPOILER', True))  # On (True) / Off (Fa
 # Verification Settings
 # ============================
 IS_VERIFY = is_enabled('IS_VERIFY', "False")  # Verification On (True) / Off (False)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '')) #Verification Channel Id 
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-100')) #Verification Channel Id 
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "")   # Tutorial link for verification
